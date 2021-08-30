ac259a31ca08e34e5a6b77fac5adf089b52a1488
deaedria <dea.edria@gmail.com>
2021-08-30 18:24:30 +0700

    fixing connect_db

--- helpers/connect_db.js
+++ helpers/connect_db.js
@@ -1,20 +1,25 @@
+const connectionString = "postgres://yatrkeovrrztfy:84fce4f88b816f0ad726b4c0c015a483d65062fb45ae1a3ec5fe93493cc39d86@ec2-18-209-153-180.compute-1.amazonaws.com:5432/d83q4s0fe7f82j"
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pg = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  ssl: true,
-  dialect: 'postgres',
-  dialectOptions: {
-    "ssl": {
-      "rejectUnauthorized": false
-    }
-  }
+  connectionString: connectionString,
+  ssl: { rejectUnauthorized: false }
 });
+// const pg = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   ssl: true,
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     "ssl": {
+//       "rejectUnauthorized": false
+//     }
+//   }
+// });
 
 pg.connect()
   .then(() => console.log("Database connected"))
