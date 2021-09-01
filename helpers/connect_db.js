7871efd6f9abac3568f1c1591a54960e2962bf53
deaedria <dea.edria@gmail.com>
2021-09-02 01:30:23 +0700

    edit env and cors

--- helpers/connect_db.js
+++ helpers/connect_db.js
@@ -1,9 +1,8 @@
-const connectionString = "postgres://yatrkeovrrztfy:84fce4f88b816f0ad726b4c0c015a483d65062fb45ae1a3ec5fe93493cc39d86@ec2-18-209-153-180.compute-1.amazonaws.com:5432/d83q4s0fe7f82j"
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pg = new Pool({
-  connectionString: connectionString,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 // const pg = new Pool({
@@ -12,13 +11,6 @@ const pg = new Pool({
 //   user: process.env.DB_USER,
 //   password: process.env.DB_PASSWORD,
 //   database: process.env.DB_DATABASE,
-//   ssl: true,
-//   dialect: 'postgres',
-//   dialectOptions: {
-//     "ssl": {
-//       "rejectUnauthorized": false
-//     }
-//   }
 // });
 
 pg.connect()
--- index.js
+++ index.js
@@ -12,14 +12,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
+app.use(cors())
 const router = require("./routes");
 router(app, "/news/api");
 
